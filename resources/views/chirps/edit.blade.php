3c16a61f6315a15a64ddacac9b752597a373e73c
yessei10 <gutierrez.rodzante@gmail.com>
2024-04-06 12:20:08 +0800

    second commit

---
+++ resources/views/chirps/edit.blade.php
@@ -0,0 +1,17 @@
+<x-app-layout>
+    <div class="max-w-2xl p-4 mx-auto sm:p-6 lg:p-8">
+        <form method="POST" action="{{ route('chirps.update', $chirp) }}">
+            @csrf
+            @method('patch')
+            <textarea
+                name="message"
+                class="block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            >{{ old('message', $chirp->message) }}</textarea>
+            <x-input-error :messages="$errors->get('message')" class="mt-2" />
+            <div class="mt-4 space-x-2">
+                <x-primary-button>{{ __('Save') }}</x-primary-button>
+                <a href="{{ route('chirps.index') }}">{{ __('Cancel') }}</a>
+            </div>
+        </form>
+    </div>
+</x-app-layout>
